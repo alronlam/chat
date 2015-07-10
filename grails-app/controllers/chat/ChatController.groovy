f2dfa3ce2b75839afcce3984b1a9ac0f70408676
Alron <alronjan@yahoo.com>
2015-07-10 09:35:46 +0800

    Added view and controller for joining the chat.

---
+++ grails-app/controllers/chat/ChatController.groovy
@@ -0,0 +1,28 @@
+package chat
+
+class ChatController {
+
+	def index() { }
+
+	def join(String name){
+		if (name.trim() == ''){
+			redirect(action: 'index')
+		}else{
+			session.name = name
+			session.ipAddress = ipAddress
+			render(view: 'chat')
+		}
+	}
+
+	def getClientIPAddress(){
+		def ipAddress = request.getHeader("Client-IP")
+
+		if (!ipAddress)
+			ipAddress = request.getHeader("X-Forwarded-For")
+
+		if (!ipAddress)
+			ipAddress = request.remoteAddr()
+
+		return ipAddress
+	}
+}
